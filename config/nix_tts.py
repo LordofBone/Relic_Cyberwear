a9f33e5c596eb4064c2672c472b5b02b4be7bfd8
314Reactor <34923578+LordofBone@users.noreply.github.com>
2022-08-16 23:48:23 +0100

    Added config for paths

--- config/nix_tts.py
+++ config/nix_tts.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+import os
 
-model_path = Path(__file__).parent.parent / f"packed_models"
+nix_dir = os.path.join(Path(__file__).parent.parent, 'nix-tts')
+audio_dir = Path(__file__).parent.parent / f"audio"
 
 determ_model_path = Path(__file__).parent.parent / f"models/deterministic"
 stoch_model_path = Path(__file__).parent.parent / f"models/stochastic"
