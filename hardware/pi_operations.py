515b43718558a1b7244ac2f0b1d46e721463e061
314Reactor <34923578+LordofBone@users.noreply.github.com>
2022-11-02 23:11:19 +0000

    Added Bot Engine

---
+++ hardware/pi_operations.py
@@ -0,0 +1,58 @@
+from events.event_queue import EventQueueAccess
+from events.event_types import SHUTDOWN, REBOOT, HARDWARE_PI
+
+from subprocess import call
+
+import logging
+
+from time import sleep
+
+logger = logging.getLogger("pi-operations")
+
+
+class PiOperations:
+    def __init__(self):
+        # todo: add extra raspberry pi operations here, temp checks etc.
+        self.shutdown_wait_seconds = 5
+
+    def shutdown(self):
+        """
+        Shutdown the Pi
+        :return:
+        """
+        logger.debug(f"Switching Off in {self.shutdown_wait_seconds} seconds")
+        sleep(self.shutdown_wait_seconds)
+        call('sudo shutdown now', shell=True)
+
+    def reboot(self):
+        """
+        Restart the Pi
+        :return:
+        """
+        logger.debug(f"Restarting in {self.shutdown_wait_seconds} seconds")
+        sleep(self.shutdown_wait_seconds)
+        call('sudo reboot now', shell=True)
+
+    def queue_checker(self):
+        """
+        Check the event queue for events
+        :return:
+        """
+        while True:
+            event = EventQueueAccess.get_latest_event([HARDWARE_PI])
+            if event:
+                if event[1] == SHUTDOWN:
+                    self.shutdown()
+                elif event[1] == REBOOT:
+                    self.reboot()
+            else:
+                sleep(1)
+
+
+PiOperationsAccess = PiOperations()
+
+if __name__ == "__main__":
+    """
+    Run the PiOperations class to test.txt LED function
+    """
+    PiOperationsAccess.shutdown()
