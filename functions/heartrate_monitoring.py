4ef9cc00c283e6d7755b5d6d10eed814187379c3
314Reactor <34923578+LordofBone@users.noreply.github.com>
2022-11-06 16:43:22 +0000

    Updated heartrate sensor and tech

--- functions/heartrate_monitoring.py
+++ functions/heartrate_monitoring.py
@@ -1,16 +1,15 @@
-from hardware.heartbeart_sensor import display_heartrate
-from hardware.pi_operations import PiOperationsAccess
-from functions.tts_operations import TTSOperationsAccess
+from time import sleep, time
 
 from config.heartrate_config import heart_check_time, time_to_switch_off
 from config.nix_tts import *
-
-
-from time import sleep, time
+from functions.tts_operations import TTSOperationsAccess
+from hardware.heartbeart_sensor import return_bpm
+from hardware.pi_operations import PiOperationsAccess
 
 
 class HeartRateMonitor:
     def __init__(self):
+        self.start_time = 0
         self.initial_time = 0
         self.last_rate = 0
         self.timeout_begun = False
@@ -19,14 +18,14 @@ class HeartRateMonitor:
 
     def check_rate(self):
         while True:
-            current_rate = display_heartrate
+            current_rate = return_bpm
 
             if not self.timeout_begun:
                 if current_rate == 0:
-                    start_time = time()
+                    self.start_time = time()
                     self.timeout_begun = True
             else:
-                if (time() - start_time < self.switch_off) and current_rate == 0:
+                if (time() - self.start_time < self.switch_off) and current_rate == 0:
                     TTSOperationsAccess.generate_tts(shutdown_text)
                     PiOperationsAccess.shutdown()
                 else:
--- hardware/heartbeart_sensor.py
+++ hardware/heartbeart_sensor.py
@@ -1,27 +1,54 @@
-#!/usr/bin/env python
-
 # NOTE! This code should not be used for medical diagnosis. It's
 # for fun/novelty use only, so bear that in mind while using it.
 
-import time
+# This code is based on the MAX30105 library examples by Pimoroni: https://github.com/pimoroni/max30105-python
+
 from max30105 import MAX30105, HeartRate
 
-max30105 = MAX30105()
-max30105.setup(leds_enable=2)
+import logging
 
-max30105.set_led_pulse_amplitude(1, 0.2)
-max30105.set_led_pulse_amplitude(2, 12.5)
-max30105.set_led_pulse_amplitude(3, 0)
+logger = logging.getLogger("heartbeart-sensor-logger")
 
-max30105.set_slot_mode(1, 'red')
-max30105.set_slot_mode(2, 'ir')
-max30105.set_slot_mode(3, 'off')
-max30105.set_slot_mode(4, 'off')
+
+class BPMSetter:
+    def __init__(self):
+        self.bpm = 0
+
+    def set_bpm(self, bpm):
+        self.bpm = bpm
+
+    def get_bpm(self):
+        return self.bpm
+
+
+def sensor_setup():
+    max30105 = MAX30105()
+
+    max30105.setup(leds_enable=2)
+
+    max30105.set_led_pulse_amplitude(1, 0.2)
+    max30105.set_led_pulse_amplitude(2, 12.5)
+    max30105.set_led_pulse_amplitude(3, 0)
+
+    max30105.set_slot_mode(1, 'red')
+    max30105.set_slot_mode(2, 'ir')
+    max30105.set_slot_mode(3, 'off')
+    max30105.set_slot_mode(4, 'off')
 
 
 def display_heartrate(beat, bpm, avg_bpm):
-    print("{} BPM: {:.2f}  AVG: {:.2f}".format("<3" if beat else "  ",
-                                               bpm, avg_bpm))
+    logger.info("{} BPM: {:.2f}  AVG: {:.2f}".format("<3" if beat else "  ", bpm, avg_bpm))
+
+    bpm_setter.set_bpm(bpm)
+
+
+def return_bpm():
+    if heartbeat_sensor_online:
+        hr.on_beat(display_heartrate, average_over=4)
+
+        return bpm_setter.get_bpm()
+    else:
+        return -1
 
 
 hr = HeartRate(max30105)
@@ -42,12 +69,11 @@ some female-to-female jumper jerky.
 https://shop.pimoroni.com/products/breakout-garden-extender-kit
 """)
 
-delay = 10
-
-print("Starting readings in {} seconds...\n".format(delay))
-time.sleep(delay)
 
 try:
-    hr.on_beat(display_heartrate, average_over=4)
-except KeyboardInterrupt:
-    pass
+    sensor_setup()
+    heartbeat_sensor_online = True
+    bpm_setter = BPMSetter()
+except ModuleNotFoundError:
+    print("No MAX30105 sensor found. Please check your wiring.")
+    heartbeat_sensor_online = False
