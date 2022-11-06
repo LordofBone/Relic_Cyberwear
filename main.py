a1aba914ab5e1b956c93293b9dd5aee2d361fe5d
314Reactor <34923578+LordofBone@users.noreply.github.com>
2022-11-06 18:37:52 +0000

    Modified boot up for heart rate init and intro sequence

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
-import sys
-import os
+import threading
+import config.submodule_dirs
 
 import logging
 
-from config.nix_tts import *
-
-from functions.tts_operations import TTSOperationsAccess
 from functions.talk_control import TalkControllerAccess
+from functions.heartrate_monitoring import HeartRateMonitorAccess
 
-soran_dir = os.path.join("soran")
+from events.event_queue import queue_adder
+from events.event_types import TALK_SYSTEMS, INTRO_SPEECH
 
-sys.path.append(soran_dir)
+logger = logging.getLogger("relic-cyberwear-system")
+
+threading.Thread(target=TalkControllerAccess.queue_checker, daemon=False).start()
 
 
 if __name__ == "__main__":
-    TTSOperationsAccess.generate_tts(boot_text)
-    # from soran.main import main
-    # main()
+    threading.Thread(target=HeartRateMonitorAccess.check_rate, daemon=False).start()
+
+    logger.info("Started Input Checking")
+
+    # Start talk control as a thread
+    threading.Thread(target=TalkControllerAccess.queue_checker, daemon=False).start()
+
+    logger.info("Started Talk Controller")
+
+    # Welcome the user to the Relic System
+    queue_adder(TALK_SYSTEMS, INTRO_SPEECH, 1)
