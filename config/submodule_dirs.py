bb7db6b808a301b7c5131ffe6dbd66df65262ef0
314Reactor <34923578+LordofBone@users.noreply.github.com>
2022-11-21 22:23:28 +0000

    Changed code to reflect removal of soran STT

--- config/submodule_dirs.py
+++ config/submodule_dirs.py
@@ -3,8 +3,8 @@ import sys
 
 bot_engine_dir = Path(__file__).parent.parent / 'Bot_Engine'
 chatbot_dir = bot_engine_dir / 'Chatbot_8'
-soran_dir = Path(__file__).parent.parent / 'soran'
+lakul_dir = Path(__file__).parent.parent / 'Lakul'
 
 sys.path.append(str(bot_engine_dir))
 sys.path.append(str(chatbot_dir))
-sys.path.append(str(soran_dir))
+sys.path.append(str(lakul_dir))
--- setup_models_and_train.py
+++ setup_models_and_train.py
@@ -1,7 +1,5 @@
 import config.submodule_dirs
 
-from soran.utils.model_downloader import download_models as download_models_soran
-
 from Bot_Engine.utils.sentiment_training_suite import train_all as train_all_sentiment
 
 from Bot_Engine.Chatbot_8.bot_8_trainer import bot_trainer as bot_trainer_8
@@ -12,7 +10,6 @@ def download_all_models_do_all_training():
     This function will download all pre-trained models/data and train the required models
     :return:
     """
-    download_models_soran()
     train_all_sentiment()
     bot_trainer_8(fresh_db=True)
 
