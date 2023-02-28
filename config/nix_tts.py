092ce3b01dee2540bd85420ee432f7cf28481c3e
314Reactor <34923578+LordofBone@users.noreply.github.com>
2023-02-28 22:14:00 +0000

    Changed boot text

--- config/nix_tts.py
+++ config/nix_tts.py
@@ -7,14 +7,11 @@ audio_dir = Path(__file__).parent.parent / f"audio"
 determ_model_path = Path(__file__).parent.parent / f"models/deterministic"
 stoch_model_path = Path(__file__).parent.parent / f"models/stochastic"
 
-boot_text = "Welcome to the Arasaka Relic Cyberware System"
-boot_text_test = "Welcome to the Arasaka Relic Cyberware System, testing mode active"
-long_boot_text = "Welcome to the Arasaka Relic Cyberware System, your consciousness is now fused with someone whos " \
-                 "mind was acquired by the soul killer software produced by the arasaka corporation, please enjoy our" \
-                 " product "
+boot_text = "Welcome to the Arasaka Relic Cyberware System; Trauma Team Edition"
+boot_text_test = "Welcome to the Arasaka Relic Cyberware System; Trauma Team Edition. Testing mode active."
 
-reboot_text = "Rebooting the system, please wait"
-shutdown_text = "Shutting down the system, please wait"
+reboot_text = "Rebooting the system, please wait."
+shutdown_text = "Shutting down the system, please wait."
 
 audio_on = True
 file_name = "tts_output.wav"
